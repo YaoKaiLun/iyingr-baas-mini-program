54670e0d0feebe54b47d60fb9e76d0cc7ec956d0
allen <yaokailun@ifanr.com>
2017-12-30 22:24:49 +0800

    add function of getting my collection of articles

--- pages/collections/index.js
+++ pages/collections/index.js
@@ -1,27 +1,60 @@
-let contentGroupID = 348
-let pageLimit = 1000
-let pageOffset = 0
+const MyContentGroup = new wx.BaaS.ContentGroup(348)
+const pageLimit = 1000
 
 Page({
   data: {
     articles: [],
+    currentUserInfo: {}
   },
-  onLoad: function () {
+
+  onLoad: function() {
+    this.getCurrentUserInfo()
     this.getArticles()
   },
-  getArticles: function () {
-    let params = {
-      contentGroupID: contentGroupID,
-      limit: pageLimit,
-      offset: pageOffset
+
+  getCurrentUserInfo: function() {
+    let currentUserInfo = wx.BaaS.storage.get('userinfo')
+    if (!currentUserInfo) {
+      wx.BaaS.login().then(res => {
+        console.log('login', res)
+        this.setData({
+          currentUserInfo: wx.BaaS.storage.get('userinfo')
+        })
+      }, err => {
+        wx.navigateTo({
+          url: `../index/index`
+        })
+      })
+    } else {
+      this.setData({currentUserInfo})
     }
-    wx.BaaS.getContentList(params).then(res => {
-      this.setData({
-        articles: res.data.objects,
+  },
+
+  getArticles: function() {
+    let Collection = new wx.BaaS.TableObject('3965')
+    let collectionQuery = new wx.BaaS.Query()
+    collectionQuery.compare('user_id', '=', wx.BaaS.storage.get('uid'))
+    Collection.setQuery(collectionQuery).limit(pageLimit).find().then(res => {
+      let contentIDs = []
+      res.data.objects.forEach(content => {
+        contentIDs.push(content.content_id)
+      })
+
+      let userInfo = this.data.currentUserInfo
+      userInfo.collectionAmount = contentIDs.length
+
+      let contentQuery = new wx.BaaS.Query()
+      contentQuery.in('id', contentIDs)
+      MyContentGroup.setQuery(contentQuery).find().then(res => {
+        this.setData({
+          articles: res.data.objects,
+          currentUserInfo: userInfo,
+        })
       })
     })
   },
-  toDetailPage: function (e) {
+
+  toDetailPage: function(e) {
     let id = e.currentTarget.dataset.id
     wx.navigateTo({
       url: `../detail/index?id=${id}`
